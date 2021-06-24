104c5ffc5267cea9b6b6ff0739a17c75e38e0d29
Chris Richmond <RichmondCJ@gmail.com>
2021-06-24 07:09:54 -0400

    Initial Commit

---
+++ LearningGitHub/LearningGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGitHubApp.swift
+//  LearningGitHub
+//
+//  Created by Mac Mini on 6/24/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
